dc00c893c4c1405e9b104540dcf54cf10b4380af
renda-doce <betoecatytavares@gmail.com>
2025-10-05 09:54:19 -0300

    Alteracao grupo Whats 05-10 p 20-10

--- js/grupo-whatsapp.js
+++ js/grupo-whatsapp.js
@@ -1,5 +1,5 @@
 // Define o link do grupo
-  const linkGrupo = "https://chat.whatsapp.com/Eyf9UgPr5FFHEbpRQi9u3J";
+  const linkGrupo = "https://chat.whatsapp.com/EIw608P7wLu3b9VEr5ab1o";
 
   // Seleciona todos os elementos com a classe
   const botoes = document.querySelectorAll(".link-grupo");
