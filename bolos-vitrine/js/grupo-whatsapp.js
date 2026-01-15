cfdf82a380129e3c355ba641342768c9c5d4b205
Roberto Tavares <roberto.tavares@trt2.jus.br>
2026-01-15 15:06:48 -0300

    Coloca evento de conversão lead na página nova

--- bolos-vitrine/js/grupo-whatsapp.js
+++ bolos-vitrine/js/grupo-whatsapp.js
@@ -1,12 +1,19 @@
 // Define o link do grupo
-  const linkGrupo = "https://chat.whatsapp.com/DacWFeD7lBC29IpWz9lPsf";
+const linkGrupo = "https://chat.whatsapp.com/DacWFeD7lBC29IpWz9lPsf";
 
-  // Seleciona todos os elementos com a classe
-  const botoes = document.querySelectorAll(".link-grupo");
+// Seleciona todos os elementos com a classe
+const botoes = document.querySelectorAll(".link-grupo");
 
-  // Adiciona o link em cada um
-  botoes.forEach(botao => {
-    botao.setAttribute("href", linkGrupo);
-    botao.setAttribute("target", "_blank"); // abre em nova aba
-    botao.setAttribute("rel", "noopener noreferrer"); // segurança
+// Adiciona o link e o rastreamento em cada um
+botoes.forEach(botao => {
+  botao.setAttribute("href", linkGrupo);
+  botao.setAttribute("target", "_blank"); // abre em nova aba
+  botao.setAttribute("rel", "noopener noreferrer"); // segurança
+
+  // Dispara evento do Pixel ao clicar
+  botao.addEventListener("click", function () {
+    if (typeof fbq === 'function') {
+      fbq('track', 'Lead');
+    }
   });
+});
