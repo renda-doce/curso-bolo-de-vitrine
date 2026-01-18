3673155a94f666efdb4dc09d42ef8b6a4990f69a
renda-doce <betoecatytavares@gmail.com>
2026-01-18 12:41:28 -0300

    Update grupo-whatsapp.js

--- js/grupo-whatsapp.js
+++ js/grupo-whatsapp.js
@@ -1,5 +1,5 @@
 // Define o link do grupo
-  const linkGrupo = "https://chat.whatsapp.com/CqPYlXJ5fh0AIiebouFrFN";
+  const linkGrupo = "https://chat.whatsapp.com/FYh55t7jQGL5Pqprogoryj";
 
   // Seleciona todos os elementos com a classe
   const botoes = document.querySelectorAll(".link-grupo");
